e26dec9a4ff378c5deb7c7aac7c9eafdefb8de9f
pavel557 <pawel.majer20@yandex.ru>
2021-03-15 15:48:11 +0000

    Задание 2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,17 @@
 #include "revert_string.h"
+#include <stdio.h>
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    char buff;
+    int lenght = strlen(str);
+    for (int i=0; i<lenght/2; i++)
+    {
+        buff = str[i];
+        str[i] = str[lenght-i-1];
+        str[lenght-i-1] = buff;
+    }
+    
 }
 
