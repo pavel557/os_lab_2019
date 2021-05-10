7c1affe548f55d9ca6f665613b71fc694f7b64f0
pavel557 <pawel.majer20@yandex.ru>
2021-05-10 13:03:10 +0000

    all

--- lab7/src/udpserver.c
+++ lab7/src/udpserver.c
@@ -7,15 +7,67 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <stdbool.h>
+#include <getopt.h>
 
-#define SERV_PORT 20001
-#define BUFSIZE 1024
 #define SADDR struct sockaddr
 #define SLEN sizeof(struct sockaddr_in)
 
-int main() {
+int main(int argc, char *argv[]) {
+  int SERV_PORT = -1;
+  int BUFSIZE = -1;  
+  while (true) {
+    int current_optind = optind ? optind : 1;
+
+    static struct option options[] = {{"SERV_PORT", required_argument, 0, 0},
+                                      {"BUFSIZE", required_argument, 0, 0},
+                                      {0, 0, 0, 0}};
+
+    int option_index = 0;
+    int c = getopt_long(argc, argv, "", options, &option_index);
+
+    if (c == -1)
+      break;
+
+    switch (c) {
+    case 0: {
+      switch (option_index) {
+      case 0:
+        SERV_PORT = atoi(optarg);
+        if (SERV_PORT <= 0) 
+            {
+                printf("SERV_PORT is a positive number\n");
+                return 1;
+            }
+        break;
+        case 1:
+        BUFSIZE = atoi(optarg);
+        if (BUFSIZE <= 0) 
+            {
+                printf("BUFSIZE is a positive number\n");
+                return 1;
+            }
+        break;
+      default:
+        printf("Index %d is out of options\n", option_index);
+      }
+    } break;
+
+    case '?':
+      printf("Unknown argument\n");
+      break;
+    default:
+      fprintf(stderr, "getopt returned character code 0%o?\n", c);
+    }
+  }
+
+  if (BUFSIZE == -1||SERV_PORT == -1) {
+    fprintf(stderr, "BUFSIZE = %d\n", BUFSIZE);
+    return 1;
+  }
   int sockfd, n;
-  char mesg[BUFSIZE], ipadr[16];
+  char* mesg = calloc(BUFSIZE, sizeof(char));
+  char ipadr[16];
   struct sockaddr_in servaddr;
   struct sockaddr_in cliaddr;
 
